eef68a6c8e627d2e75d8c1db8df446fd11ae5268
David Chaloupka <chaloupka.david@gmail.com>
2015-07-28 10:24:31 +0200

    Setting version to 0.1.4 to start development of this version.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject log-watchdog "0.1.3-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject log-watchdog "0.1.4-SNAPSHOT"
+  :description "Watches log files for suspicious lines and notifies when something is found."
+  :url "https://github.com/kvafy/log-watchdog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
